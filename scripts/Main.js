da3e7701bb15135bcb41ebaf4b3b40ca05c9730c
Amin Dana <AminDanaaa@Gmail.com>
2026-09-06 20:58:56 +0330

    game logic works in console atm

--- scripts/Main.js
+++ scripts/Main.js
@@ -8,8 +8,10 @@ function createPlayer(playerName, playerMarker) {
     return { getName, getMarker };
 }
 
+
+
 const board = (() => {
-    let boardArray = Array(9).fill(null);
+    let boardArray = Array(9).fill("");
 
     const WIN_PATTERNS = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],    // Rows
@@ -17,4 +19,86 @@ const board = (() => {
         [0, 4, 8], [2, 4, 6]                // Diagonals
     ];
 
+    // Always returns "X" or "O" (True values) or "" (Falsy value)
+    const searchForWinner = () => {
+        printBoardConsole();
+        for (let pattern of WIN_PATTERNS) {
+            const [a, b, c] = pattern;
+            if (boardArray[a] && boardArray[a] === boardArray[b] && boardArray[a] === boardArray[c]) {
+                return boardArray[a];
+            }
+        }
+        return "";
+    };
+
+    const printBoardConsole = () => {
+        console.log(`"${boardArray[0]}"   "${boardArray[1]}"   "${boardArray[2]}"`);
+        console.log(`"${boardArray[3]}"   "${boardArray[4]}"   "${boardArray[5]}"`);
+        console.log(`"${boardArray[6]}"   "${boardArray[7]}"   "${boardArray[8]}"`);
+    }
+
+    const placeMarker = (index, marker) => {
+        if (boardArray[index] === "") {
+            boardArray[index] = marker;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const resetBoard = () => {
+        boardArray = Array(9).fill("");
+    };
+
+    return { searchForWinner, resetBoard, placeMarker };
 })();
+
+
+
+const gameControl = (() => {
+    let moves = 0;
+
+    const playRound = () => {
+        moves++;
+        let searchResult = board.searchForWinner();
+        if (searchResult) {
+            resetGame();
+            return `The ${searchResult} has won!`;
+        }
+        if (moves === 9 && !searchResult) {
+            resetGame();
+            return "The game is a tie.";
+        }
+    };
+
+    const resetGame = () => {
+        board.resetBoard();
+        moves = 0;
+    };
+
+    return { playRound, resetGame };
+})();
+
+
+
+const player1 = createPlayer("Amin", "X");
+const player2 = createPlayer("Someone", "O");
+
+board.placeMarker(2, player1.getMarker());
+console.log(gameControl.playRound());
+board.placeMarker(1, player2.getMarker());
+console.log(gameControl.playRound());
+board.placeMarker(7, player1.getMarker());
+console.log(gameControl.playRound());
+board.placeMarker(4, player2.getMarker());
+console.log(gameControl.playRound());
+board.placeMarker(6, player1.getMarker());
+console.log(gameControl.playRound());
+board.placeMarker(3, player2.getMarker());
+console.log(gameControl.playRound());
+board.placeMarker(0, player1.getMarker());
+console.log(gameControl.playRound());
+board.placeMarker(8, player2.getMarker());
+console.log(gameControl.playRound());
+board.placeMarker(5, player1.getMarker());
+console.log(gameControl.playRound());
