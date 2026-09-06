a3754b9072056ef39b2a55a1561b409b0df4dec7
Amin Dana <AminDanaaa@Gmail.com>
2026-09-06 19:15:27 +0330

    update createPlayer function and make board IIFE object

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,8 +1,20 @@
-function createPlayer(name) {
-    let score = 0;
-    const getScore = () => score;
-    const giveScore = () => { score++; };
-    const resetScore = () => { score = 0; };
+function createPlayer(playerName, playerMarker) {
+    let name = playerName;
+    let marker = playerMarker;
 
-    return { name, getScore, giveScore, resetScore };
+    const getName = () => name;
+    const getMarker = () => marker;
+
+    return { getName, getMarker };
 }
+
+const board = (() => {
+    let boardArray = Array(9).fill(null);
+
+    const WIN_PATTERNS = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],    // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],    // Columns
+        [0, 4, 8], [2, 4, 6]                // Diagonals
+    ];
+
+})();
